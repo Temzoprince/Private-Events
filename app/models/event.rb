5ced97c7ddee7b474a8008a19e554963a3cce420
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-07-29 23:25:27 +0100

    Event class methods for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances
   has_many :attendees, through: :attendances
+
+  def self.past
+    all.select { |event| event.date < Date.current }
+  end
+
+  def self.upcoming
+    all.select { |event| event.date >= Date.current }
+  end
 end
